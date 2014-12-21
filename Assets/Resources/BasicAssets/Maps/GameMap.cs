f8d9f1d900c4163a78a4c893f15374802217ca28
Neobii <adamlove@goodybag.com>
2014-12-21 06:04:40 -0600

    fix stupid syntax error, just in case anyone carebearz.

--- Assets/Resources/BasicAssets/Maps/GameMap.cs
+++ Assets/Resources/BasicAssets/Maps/GameMap.cs
@@ -5,7 +5,7 @@ public class GameMap : MonoBehaviour {
 	public object spawnPoints;
 	// Use this for initialization
 	void Start () {
-		spawnPoints = this.gameObject.
+		//spawnPoints = this.gameObject.
 	}
 	
 	// Update is called once per frame
